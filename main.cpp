6677d3b52f5d7b0f05546142f58ed3786be9f9e8
Veasst <veasst95@gmail.com>
2019-12-22 13:29:14 +0100

    Connect to and handshake with Discord's WebSocket

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 #include <iostream>
 #include <string>
 
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
 #include <curl/curl.h>
 
-size_t writeResponse(char* contents, size_t size, size_t numberOfElements, void* destination)
+size_t writeResponse(const char* source, const size_t size, const size_t numberOfElements, void* destination)
 {
-	((std::string*)destination)->append((char*)contents, size * numberOfElements);
+	((std::string*)destination)->append((char*)source, size * numberOfElements);
 	return size * numberOfElements;
 }
 
@@ -40,13 +47,12 @@ std::string getGatewayUrl()
 	boost::property_tree::read_json(is, ptree);
 
 	auto gatewayUrl = ptree.get_optional<std::string>("url");
-	
 	if (!gatewayUrl)
 	{
 		return "";
 	}
 
-	return *gatewayUrl;
+	return (*gatewayUrl).substr(6, (*gatewayUrl).size() - 6);
 }
 
 int main()
@@ -57,7 +63,22 @@ int main()
 		return 0;
 	}
 
-	std::cout << gatewayUrl;
+	boost::asio::io_context ioc;
+	boost::asio::ip::tcp::resolver resolver{ ioc };
+	boost::asio::ssl::context sslcontext{ boost::asio::ssl::context::tlsv13_client };
+	boost::beast::websocket::stream<boost::beast::ssl_stream<boost::asio::ip::tcp::socket>> websocket{ ioc, sslcontext };
+
+	try
+	{
+		auto const endpoints = resolver.resolve(gatewayUrl, "443");
+		boost::asio::connect(websocket.next_layer().next_layer(), endpoints);
+		websocket.next_layer().handshake(boost::asio::ssl::stream_base::client);
+		websocket.handshake(gatewayUrl, "/?v=6&encoding=json");
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what();
+	}
 
 	return 0;
 }
